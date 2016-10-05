2507a57e39e1d2936bd2d39c76df5f66cc66a968
Dominic Serrano <dmsrojo@gmail.com>
2016-10-04 20:27:43 -0500

    Now only the current uploaded file is being save to local memory

--- server.js
+++ server.js
@@ -2,31 +2,44 @@
 
 const express = require('express');
 const app = express();
-const { createReadStream, createWriteStream, readFile } = require('fs');
+const { createReadStream, createWriteStream, rmdirSync, readdirSync, unlinkSync } = require('fs');
 // const readStream = createReadStream('yoshi.jpg');
 const bodyparser = require('body-parser');
 const multer = require('multer');
 /////////////////////////////////////////
-
+//Destination to upload file
 const upload = multer({
   dest: __dirname + '/partials/uploads/',
 });
+//Store the current uploads dir
+const uploadsDir = readdirSync('./partials/uploads');
 
+
+//Middle wares
 app.use(express.static(__dirname + '/partials/'));
 
-
+//Post uploaded img to this route
 app.post('/uploads', upload.single('image'), (req, res) => {
   let img64 = "";
 
+  //Create readStream to encode img into base64 string
   createReadStream(`./partials/uploads/${req.file.filename}`, { encoding: 'base64'})
   .on('data', (buffer) => {
     img64 += buffer;
   })
   .on('end', () => {
     console.log("Test end");
+    
+    res.send(`<img src="data:image/jpg;base64,${img64}" />`);
 
-    res.send(`<img src="data:image/jpg;base64,${img64}" />`)
-  })
+    if (uploadsDir.length === 1) {
+      uploadsDir.forEach((each) => {
+        unlinkSync(`./partials/uploads/${each}`);
+      });
+    };
+
+  });
+
 
 });
 
